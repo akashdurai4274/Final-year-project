5c5aa47d7c2fb952519a21b716c08403979bcd56
AkashDurai <93862553+akashdurai4274@users.noreply.github.com>
2024-04-09 10:38:08 +0530

    Delete lib directory

--- lib/Authentication/config.dart
+++
@@ -1,5 +0,0 @@
-final url = "http://192.168.142.167:3000/";
-final register = url + "registration";
-final login = url + "login";
-final classobj = url+"oops/classobj/";
-final coding = url+"oops/classobj/coding";
